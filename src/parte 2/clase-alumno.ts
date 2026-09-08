98bb17e329ffe91598814d190470a1f9d4e87a1c
IgnisNux <campionrexprohard@gmail.com>
2026-09-08 01:51:39 -0300

    Ejercicios 8 y 9 Parte 2 y ejercicio 17 parte 3

--- src/parte 2/clase-alumno.ts
+++ src/parte 2/clase-alumno.ts
@@ -39,7 +39,11 @@ export class Alumno {
         email: string
     ) {
         // TODO (Ejercicio 8): asignar los atributos recibidos.
-        throw new Error("Implementar");
+        this.legajo = legajo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.setEdad(edad);
     }
 
     // -------------------------------------------------------------------
