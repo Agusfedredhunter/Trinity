1c356a9702ed3b42eaa267333f7795d9714d431f
Iñaki Urdampilleta <iniakiur@hotmail.com>
2026-09-08 14:54:29 -0300

    Actualizacion Ejercicio 10 de la parte 2
    Avance en el trabajo practico, modificacion en el ejercicio 10 de la parte 2 que esta en progreso (aun faltan modificar cosas del ejercicio 8 y 9)

--- src/parte 2/clase-alumno.ts
+++ src/parte 2/clase-alumno.ts
@@ -81,29 +81,28 @@ export class Alumno {
     // -------------------------------------------------------------------
 
     agregarMateria(materia: Materia): void {
-        // TODO
-        throw new Error("Implementar");
+        if (!this.estaInscripto(materia.codigo)) {
+            this.materias.push(materia);
+        }
     }
 
     quitarMateria(codigo: number): Materia | undefined {
-        // TODO: quitar la materia con ese código y devolverla.
-        // Si no está inscripto en ninguna con ese código, devolver undefined.
-        throw new Error("Implementar");
+        const indice = this.materias.findIndex((materia) => materia.codigo === codigo);
+        if (indice === -1) {
+            return undefined;
+        }
+        return this.materias.splice(indice, 1)[0];
     }
 
     estaInscripto(codigo: number): boolean {
-        // TODO
-        throw new Error("Implementar");
+        return this.materias.some((materia) => materia.codigo === codigo);
     }
 
     cantidadMaterias(): number {
-        // TODO
-        throw new Error("Implementar");
+        return this.materias.length;
     }
 
     getMaterias(): Materia[] {
-        // TODO: devolver las materias sin exponer el arreglo interno
-        // (devolver una copia, no la referencia original).
-        throw new Error("Implementar");
+        return [...this.materias];
     }
 }
